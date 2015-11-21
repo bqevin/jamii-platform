763cdca679eb545977850d6d0ce65b1b95a97e2f
Kowsheek Mahmood <kowsheek@gmail.com>
2015-11-21 10:55:31 -0500

    Setup base project
    issue #1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # jamii
+
+1. Install NodeJS
+2. Install NPM
+3. Install MongoDB and start it up
+4. run `grunt serve` in the directory
+	
+	
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/jamii-dev'
+  },
+
+  seedDB: true
+};
