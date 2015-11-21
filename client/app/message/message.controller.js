18f185e5b73e2ea5a3caae59344aaa1bf2adf582
Dariusz Grabka <dariusz@tulip.io>
2015-11-21 13:17:18 -0500

    Added twilio lib.

--- client/app/message/message.controller.js
+++ client/app/message/message.controller.js
@@ -2,7 +2,13 @@
 
 angular.module('jamiiApp')
   .controller('MessageCtrl', function ($scope, $http) {
+
     $http.get('/api/messages').then(function (res) {
       $scope.messages = res.data;
     });
+
+    $scope.sendMessage = function() {
+      alert("Hey!");
+    }
+
   });
