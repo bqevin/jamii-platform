a37a371e4cbdf1dd52c9fbc73e36b97180859713
Dariusz Grabka <dariusz@tulip.io>
2015-11-21 16:55:08 -0500

    channel detail view

--- client/app/channel/channel.detail.controller.js
+++ client/app/channel/channel.detail.controller.js
@@ -2,6 +2,8 @@
 
 angular.module('jamiiApp')
   .controller('ChannelDetailCtrl', function ($scope, $http, channelId) {
+      $scope.channel = {};
+      $scope.channel.members = [];
     $http.get('/api/channels/' + channelId).then(function (res) {
       $scope.channel = res.data;
     });
